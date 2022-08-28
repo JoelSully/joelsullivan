c509be3c07cbca3203b2a63ad4fd39d0f6287ab5
Joel <joel@Amandas-MacBook-Air.local>
2022-08-27 22:07:28 -0500

    added some js & stimulus

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,7 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
 
+   const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 721,
+    effect: 'cube',
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+
 export default class extends Controller {
-  connect() {
-    this.element.textContent = "Hello World!"
-  }
+  
 }

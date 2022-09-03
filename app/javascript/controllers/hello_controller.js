fb43b843e7a9af97a7fb430dc8c7c14eed8057af
Joel <joel@Amandas-MacBook-Air.local>
2022-09-03 01:09:33 -0500

    removed swiper API and actions

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,22 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
 
-   const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 600,
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-
-  app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
-
 
 export default class extends Controller {
   

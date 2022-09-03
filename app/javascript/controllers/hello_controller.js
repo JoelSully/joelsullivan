77d0856815baad10df35e4915b6d78a94b13dad2
Joel <joel@Amandas-MacBook-Air.local>
2022-09-03 18:23:18 -0500

    new

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,19 +1,1 @@
-import { Controller } from "@hotwired/stimulus"
 
-   const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
-    speed: 600,
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    }
-  });
-
-
-export default class extends Controller {
-  
-}

8a958ec7ac785ec9b5d0c61eaddb2808dfccdd9a
Joel <joel@Amandas-MacBook-Air.local>
2022-09-03 01:07:11 -0500

    added listen

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -13,6 +13,10 @@ import { Controller } from "@hotwired/stimulus"
     }
   });
 
+  app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+
 
 export default class extends Controller {
   

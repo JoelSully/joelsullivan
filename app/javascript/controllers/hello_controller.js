57bae468894a8d2bedb7a06e2cdba277d544da4a
Joel <joel@Amandas-MacBook-Air.local>
2022-09-03 17:10:01 -0500

    updated swiper and scrollreveal

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -1,5 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
 
+   const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    speed: 600,
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
 
 export default class extends Controller {
   
